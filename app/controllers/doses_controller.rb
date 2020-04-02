d591ba4a3c9b46ef66b0a9887c853fe0d11d9930
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-04-02 16:50:16 +0200

    mister cocktail route faites mais controller erreur

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_url, notice: 'dose was successfully destroyed.'
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/cocktails", to: 'cocktails#index'
+  post '/cocktails', to: 'cocktails#create'
+  get '/cocktails/new', to: 'cocktails#new'
+  get '/cocktails/:id', to: 'cocktails#show'
+  patch '/cocktails/:id', to: 'cocktails#update'
+  delete '/cocktails/:id', to: 'cocktails#destroy'
+  get '/cocktails/:id/edit', to: 'cocktails#edit'
+
+  post '/cocktails/:cocktail_id/doses', to: 'doses#create'
+  get '/cocktails/:cocktail_id/doses/new', to: 'doses#new'
+  delete '/doses/:id', to: 'doses#destroy'
 end
