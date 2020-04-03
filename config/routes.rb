0b26f28441ca728b854b4a6bef76d7661bb537a7
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-04-03 14:42:50 +0200

    jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get "/cocktails", to: 'cocktails#index'
+  root to: "cocktails#index"
   post '/cocktails', to: 'cocktails#create'
   get '/cocktails/new', to: 'cocktails#new'
   get '/cocktails/:id', to: 'cocktails#show'
